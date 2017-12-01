ba1ac4f1f6acafb77e7ba59e6103b2a454f031db
Yanick <rt-2@rt-2.net>
2017-12-01 13:16:41 -0500

    New methods
    New methods to prevent error near the pole (glimbal lock)

--- quaternion2euler.inc.php
+++ quaternion2euler.inc.php
@@ -4,31 +4,47 @@
 //      Created: 2017/11/30
 //      Url: https://github.com/rt-2/PHP-Quaternion2Euler
 //
-
 function Quaternion2Euler($x, $y, $z, $w) {
-    $ysqr = $y * $y;
 
-    $t0 = +2.0 * ($w * $x + $y * $z);
-    $t1 = +1.0 - 2.0 * ($x * $x + $ysqr);
-    $ret_x = rad2deg(atan2($t0, $t1));
+    $heading = 0;
+    $attitude = 0;
+    $bank = 0;
 
-    $t2 = +2.0 * ($w * $y - $z * $x);
-    $t2 = $t2 > 1.0 ? 1.0 : $t2;
-    $t2 = $t2 > +1.0 ? +1.0 : $t2;
-    $t2 = $t2 < -1.0 ? -1.0 : $t2;
-    $ret_y = rad2deg(asin($t2));
+    $qw = (float)$w;
+    $qx = (float)$x;
+    $qy = (float)$y;
+    $qz = (float)$z;
 
-    $t3 = +2.0 * ($w * $z + $x * $y);
-    $t4 = +1.0 - 2.0 * ($ysqr + $z * $z);
-    $ret_z = rad2deg(atan2($t3, $t4));
+    $test= $qx*$qy + $qz*$qw;
 
-    //echo "x:$x, y:$y, z:$z, w:$w returned x:$ret_x, y:$ret_y, z:$ret_z;";
-    //echo '<br><br>';
+    $qw2 = $qw*$qw;
+    $qx2 = $qx*$qx;
+    $qy2 = $qy*$qy;
+    $qz2 = $qz*$qz;
+
+    if ($test > 0.499) {
+      $heading = 360 / pi() * atan2($qx,$qw);
+      $attitude = 90;
+      $bank = 0;
+    }
+    elseif ($test < -0.499) {
+      $heading = -360 / pi() * atan2($qx,$qw);
+      $attitude = -90;
+      $bank = 0;
+    }
+    else{
+        $h = atan2( 2*$qy*$qw-2*$qx*$qz , 1 - 2 * $qy2-2*$qz2 );
+        $a = asin( 2*$qx*$qy + 2*$qz*$qw );
+        $b = atan2( 2*$qx*$qw-2*$qy*$qz , 1-2*$qx2-2*$qz2 );
+        $heading = $h * 180 / pi();
+        $attitude = $a * 180 / pi();
+        $bank = $b * 180 / pi();
+    }
 
     return (object)[
-        'x' => $ret_x,
-        'y' => $ret_y,
-        'z' => $ret_z
+      'x' => $bank,
+      'y' => $attitude,
+      'z' => $heading
     ];
-    
+
 }
